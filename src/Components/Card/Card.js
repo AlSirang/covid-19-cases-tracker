1a715e391c0d7d3c9c10a0c3d1a82f17da9c1e5c
bashir uddin <uddinbashir1999@gmail.com>
2021-02-13 21:08:34 +0500

    Create Card Component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,4 +1,5 @@
 import { Typography, Paper } from "@material-ui/core";
+import CountUp from "react-countup";
 import "./Card.css";
 
 export const Card = ({ name, count, lastUpdate }) => {
@@ -8,10 +9,10 @@ export const Card = ({ name, count, lastUpdate }) => {
                 <Typography variant="h3" className={name}>
                     {name}
                 </Typography>
-                <Typography variant="h6">{count}</Typography>
-                <Typography>
-                    {(new Date(lastUpdate)).toUTCString()}
+                <Typography variant="h6">
+                    <CountUp start={0} end={count} delay={0.5} separator="," />
                 </Typography>
+                <Typography>{new Date(lastUpdate).toDateString()}</Typography>
             </Paper>
         </div>
     );
