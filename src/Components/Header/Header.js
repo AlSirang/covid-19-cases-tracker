ce8a735d430a3eb977837226b91c7d18e9c38208
bashir uddin <uddinbashir1999@gmail.com>
2021-02-14 03:24:28 +0500

    project completes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,44 @@
-import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, InputBase } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import { useState } from "react";
+import { useStyles } from "./HeaderStyles";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-
-    title: {
-        flexGrow: 1,
-        display: "none",
-        [theme.breakpoints.up("sm")]: {
-            display: "block",
-        },
-    },
-}));
-export const Header = () => {
-    const classes = useStyles;
+export const Header = ({ handleCountry }) => {
+    const classes = useStyles();
+    const [usrInput, setInput] = useState("");
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    };
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
                     <Typography className={classes.title} variant="h5" noWrap>
-                        Covid-19 Tracker
+                        Covid-19 Cases Tracker
                     </Typography>
+                    <div className={classes.search}>
+                        <form
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                handleCountry(usrInput);
+                            }}>
+                            <div className={classes.searchIcon}></div>
+
+                            <InputBase
+                                placeholder="Country..."
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                                onChange={handleChange}
+                                value={usrInput}
+                                inputProps={{ "aria-label": "search" }}
+                            />
+                            <button type="submit" className={classes._btn}>
+                                <SearchIcon />
+                            </button>
+                        </form>
+                    </div>
                 </Toolbar>
             </AppBar>
         </div>
